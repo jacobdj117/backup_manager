ac8ce46d0445d1f95c5ad4c5e84781655c57c063
Jacob <jacobdj117@gmail.com>
2024-11-27 08:25:54 -0600

    now creates file if it doesn't exist.  Adds baseline file rading and writing capabilities

--- backup_manager.py
+++ backup_manager.py
@@ -5,14 +5,16 @@ from program.file_manager import file_manager
 
 arguments = arguments(sys.argv)
 
-if arguments.config_name != "":
-    file_manager = file_manager()
-    file_manager.initialize_config(
-        arguments.config_name,
-        arguments.save_config)
-else:
+if arguments.config_name == "":
     file_manager = file_manager(
         arguments.source_files,
         arguments.source_directories,
         arguments.output_directories
     )
+else:
+    file_manager = file_manager()
+    file_manager.initialize_config(
+        arguments.config_name
+    )
+
+file_manager.perform_copies()
--- program/file_manager.py
+++ program/file_manager.py
@@ -1,6 +1,8 @@
 import shutil
 
 class file_manager:
+    _CONFIG_FILE_ = "configs.csv"
+
     def __init__(self, source_files, sourcce_directories, ourput_directories):
         self.source_files = source_files
         self.source_directories = sourcce_directories
@@ -23,10 +25,19 @@ class file_manager:
                 shutil.copytree(directory, destination)
 
     def initialize_config(self, config_name):
-        return
+        self.config_file = open(self._CONFIG_FILE_, "a+")
+        self.config_file.seek(0)
 
-    def create_config(self, config_name):
-        return
+        if not self.read_config(config_name):
+            self.create_config(config_name)
+
+        self.config_file.close()
+
+    def read_config(self, config_name):
+        for line in self.config_file:
+            print(line)
+        return False
     
-    def load_config(self, config_name):
+    def create_config(self, config_name):
+        self.config_file.write(config_name + "\n")
         return
