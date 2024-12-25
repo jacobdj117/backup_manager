f2958d52783222c250445f1bf57f507dbf9a2a6a
Jacob <jacobdj117@gmail.com>
2024-12-25 15:47:03 -0600

    Adds help command

--- program/arguments.py
+++ program/arguments.py
@@ -31,6 +31,9 @@ class arguments:
                 if index + 1 >= arg_count: return
                 self.source_files.append(self.args[index + 1])
                 index = index + 2
+            elif arg == "-h" or arg == "--help":
+                self.show_help()
+                exit()
             elif arg == "-l" or arg == "--list":
                 self.list_configs = True
                 index = index + 1
@@ -41,3 +44,22 @@ class arguments:
             
             else:
                 index = index + 1
+    
+    def show_help(self):
+        print("-c  --config    | Specify the name of a config to run.  If the config exists,")
+        print("                | the source and output paths stored in it will be used.  If")
+        print("                | the config does not exist, a new config with the specified")
+        print("                | name will be created using the -d, -f and -o arguments provided")
+        print("-d  --directory | A directory to be backed up.  Specify the whole path.  If the")
+        print("                | path containes spaces, it must be enclosed in quotes.  Multiple")
+        print("                | directories may be specified by using multiple -d arguments")
+        print("-f  --file      | A file to be backed up.  Specify the whole path.  If the")
+        print("                | path containes spaces, it must be enclosed in quotes.  Multiple")
+        print("                | paths may be specified by using multiple -f arguments")
+        print("-h  --help      | Display this help readout")
+        print("-l  --list      | Display the names of all existing configs")
+        print("-o  --output    | A path to place the specified input files and directories.")
+        print("                | Specify the whole path.  If the path contains spaces, enclose")
+        print("                | it in quotes.  Multiple outputs may be specified bu using")
+        print("                | -o arguments")
+        
